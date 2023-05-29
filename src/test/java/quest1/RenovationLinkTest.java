4e2688a20fdbb31af0de9f2989834a013ad624f7
Viktor Lukashev <lukashevviktor@gmail.com>
2023-05-29 15:58:48 +0300

    Обновил RenovationLinkTest.java

--- src/test/java/quest1/RenovationLinkTest.java
+++ src/test/java/quest1/RenovationLinkTest.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class RenovationLinkTest extends BaseSeleniumTest {
-    private final By renovation = By.xpath("//a[contains(.,'Renovation')]");
+    private final By renovation = By.linkText("Renovation");
 
     @DisplayName("Кликнуть на первую ссылку с текстом «Renovation»")
     @Test
     public void firstLinkRenovationTest() {
-        String textNoClick = driver.getTitle();
         driver.findElement(renovation).click();
         String textAftrClick = driver.getTitle();
-        System.out.println(textAftrClick);
         Assertions.assertEquals("Ремонт квартир на Коста-дель-Соль", textAftrClick);
     }
 
