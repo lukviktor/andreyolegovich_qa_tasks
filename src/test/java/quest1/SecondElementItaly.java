70ed4333988334ee90a527d47e3ef78c0f966b21
Viktor Lukashev <lukashevviktor@gmail.com>
2023-05-29 15:57:25 +0300

    Справились с SecondElementItaly.java

--- src/test/java/quest1/SecondElementItaly.java
+++ src/test/java/quest1/SecondElementItaly.java
@@ -1,5 +1,18 @@
 package quest1;
 
-public class SecondElementItaly {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
+public class SecondElementItaly extends BaseSeleniumTest{
+    private final By italy = By.cssSelector("a.march8:nth-of-type(1)");
+    @Test
+    public void italyTest() {
+        WebElement element = driver.findElement(italy);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+        Assertions.assertTrue(driver.getTitle().contains("8 марта в Италии"));
+    }
 }
